6edadea6f8cba36ec183ae42528e8cbb2e83a224
Yuriyli <0yuriyli@gmail.com>
2024-07-31 20:34:26 +0300

    Миграция последний изменений в модели

--- CosmeticCatalog/Migrations/20240731172819_CatalogModelsFix.Designer.cs
+++ CosmeticCatalog/Migrations/20240731172819_CatalogModelsFix.Designer.cs
@@ -4,6 +4,7 @@ using CosmeticCatalog.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CosmeticCatalog.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240731172819_CatalogModelsFix")]
+    partial class CatalogModelsFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CosmeticCatalog/Migrations/20240731172819_CatalogModelsFix.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CosmeticCatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class CatalogModelsFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ComponentProduct",
+                columns: table => new
+                {
+                    ComponentsId = table.Column<int>(type: "int", nullable: false),
+                    ProductsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComponentProduct", x => new { x.ComponentsId, x.ProductsId });
+                    table.ForeignKey(
+                        name: "FK_ComponentProduct_Components_ComponentsId",
+                        column: x => x.ComponentsId,
+                        principalTable: "Components",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ComponentProduct_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComponentProduct_ProductsId",
+                table: "ComponentProduct",
+                column: "ProductsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ComponentProduct");
+        }
+    }
+}
