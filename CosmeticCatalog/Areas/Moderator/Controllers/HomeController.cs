73d3f38fd4735adacb10b31e684cafe90d0f2e7c
Yuriyli <0yuriyli@gmail.com>
2024-08-05 12:15:31 +0300

    Пустой щаблон области /Moderator/

---
+++ CosmeticCatalog/Areas/Moderator/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CosmeticCatalog.Areas.Moderator.Controllers
+{
+    [Area("Moderator")]
+    [Authorize(Roles = "Moderator")]
+    public class HomeController : Controller
+    {
+        [Route("{area}")]
+        [HttpGet]
+        public IActionResult Index(int categoryId)
+        {
+            return View();
+        }
+    }
+}
