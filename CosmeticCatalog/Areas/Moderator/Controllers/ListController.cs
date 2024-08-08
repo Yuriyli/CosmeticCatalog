5722f6c95ffe5b79a062a0c4b84587a20d7a2d93
Yuriyli <0yuriyli@gmail.com>
2024-08-08 14:49:08 +0300

    Навигация по Area/Moderator
    -Views/Shared/_RoleNavigationPartial.cshtml меню для роли "Moderator"
    -Сигнатуры для методов контроллеров в области Moderator
    -Пустые View для соответсвующих методов

---
+++ CosmeticCatalog/Areas/Moderator/Controllers/ListController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CosmeticCatalog.Areas.Moderator.Controllers
+{
+    [Area("Moderator")]
+    [Authorize(Roles = "Moderator")]    
+    public class ListController : Controller
+    {
+        [Route("{area}/{controller}/Category")]
+        public IActionResult Category()
+        {
+            return View();
+        }
+        [Route("{area}/{controller}/Component")]
+        public IActionResult Component()
+        {
+            return View();
+        }
+        [Route("{area}/{controller}/Product")]
+        public IActionResult Product()
+        {
+            return View();
+        }
+        [Route("{area}/{controller}/Tag")]
+        public IActionResult Tag()
+        {
+            return View();
+        }
+    }
+}
